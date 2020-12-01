5888018b1606ba9a09afa4cc44614e1561bad201
Shing <shingso026@gmail.com>
2020-12-01 05:27:05 -0800

    Fixed updateUserFunction to not update instead of set

--- helperFunctions.js
+++ helperFunctions.js
@@ -3,7 +3,8 @@ import { v4 as uuid } from 'uuid'
 
 
 export async function addUser(userID) {
-    const ref = firestore().collection('Users').doc(userID)
+    
+  const ref = firestore().collection('Users').doc(userID)
     await ref.set({
       timeStamp: firestore.FieldValue.serverTimestamp(),
       currentStreak: 0,
@@ -11,13 +12,7 @@ export async function addUser(userID) {
       highestStreak: 0,
       lastStudied: firestore.FieldValue.serverTimestamp(),
     });
-    // Add collection datesstudied
-    // everytime studied is complete add to dates studied
-    // await ref.add({
-    //   
-    //   
-    //   timeStamp: firestore.FieldValue.serverTimestamp(),
-    // });
+ 
    
   }
 
@@ -29,8 +24,6 @@ export async function updateUserInfo(userID) {
     await ref.update({
       timeStamp: firestore.FieldValue.serverTimestamp()
     });
-
-  //population: firebase.firestore.FieldValue.increment(1) - inorder to increament 
    
 }
 
@@ -39,20 +32,13 @@ export async function updateUserStreakData(userID, IQ, currentStreak, highestStr
   const batch = firestore().batch();
   const ref = firestore().collection('Users').doc(userID)
   const ref2 = firestore().collection('Users').doc(userID).collection('DatesStudied').doc(docID)
+  
   batch.update(ref,{
     lastStudied: firestore.FieldValue.serverTimestamp(),
     IQ: IQ,
     currentStreak: currentStreak,
     highestStreak: highestStreak
   });
-  
-  batch.set(ref,{
-    lastStudied: firestore.FieldValue.serverTimestamp(),
-    IQ: IQ,
-    currentStreak: currentStreak,
-    highestStreak: highestStreak
-  });
-
   
   batch.set(ref2, {   
      timeStamp: firestore.FieldValue.serverTimestamp(),
@@ -65,22 +51,6 @@ export async function updateUserStreakData(userID, IQ, currentStreak, highestStr
 
   batch.commit()
 
-/*   await ref.update({
-    lastStudied: firestore.FieldValue.serverTimestamp(),
-    IQ: IQ,
-    currentStreak: currentStreak,
-    highestStreak: highestStreak
-  });
-
-
-  await ref.add({   
-    timeStamp: firestore.FieldValue.serverTimestamp(),
-  }); */
-
-
-//population: firebase.firestore.FieldValue.increment(1) - inorder to increament 
-//here we want to keep track of the users data 
- 
 }
 
 
