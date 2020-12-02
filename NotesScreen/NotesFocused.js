0e9e60fff24ad8849a334317f56511697f83ee4e
Shing So <shingso@Shings-MacBook-Pro.local>
2020-12-01 18:34:00 -0800

    Added Delete function and confirmation modal

--- NotesScreen/NotesFocused.js
+++ NotesScreen/NotesFocused.js
@@ -3,13 +3,13 @@ import { View, StyleSheet, SafeAreaView } from 'react-native'
 import firestore from '@react-native-firebase/firestore';
 import { format } from 'date-fns'
 import { AuthContext } from '../AuthContext'
-import { Card, List, Text, Button, Icon, TopNavigation, TopNavigationAction } from '@ui-kitten/components';
+import { Card, List, Text, Button, Icon, Modal, Input } from '@ui-kitten/components';
 import Empty from '../UtilComponents/EmptyList'
 import TopHeader from '../UtilComponents/TopHeader'
 
 
-const BackIcon = (props) => (
-  <Icon {...props} name='arrow-back' />
+const TrashIcon = (props) => (
+  <Icon {...props} width={27} height={27} name='trash-2-outline' />
 );
 
 
@@ -22,7 +22,9 @@ function NotesFocused({ route, navigation }){
     const { title } = route.params
     const [ loading, setLoading ] = useState(true);
     const [ todos, setTodos ] = useState([]);
-    
+    const [ visible, setVisible ] = useState(false)
+    const [ value, setValue ] = React.useState('');
+
     const renderItem = (info) => (
       
       <View style={styles.item}>
@@ -37,8 +39,11 @@ function NotesFocused({ route, navigation }){
     const renderHeader = () => (
         
       <View>
+      <View style={{flexDirection:'row', justifyContent:'space-between', alignItems:'center'}}>
       <TopHeader/>
-      <Text category='s1'>{title}</Text>
+      <Button style={{marginRight:-12}} size='small' appearance='ghost' accessoryLeft={TrashIcon} onPress={()=>setVisible(true)}></Button>
+      </View>
+      <Text style={{marginTop:12}} category='s1'>{title}</Text>
       </View>
     );
  
@@ -91,7 +96,34 @@ function NotesFocused({ route, navigation }){
          ListEmptyComponent={<Empty message={'Your notes will be placed here'}/>}
          ListHeaderComponent={renderHeader}
          />
- 
+
+        <Modal
+          visible={visible}
+          backdropStyle={styles.backdrop}
+          >
+          <Card style={{marginHorizontal:40}} disabled={true}>
+          
+          <View style={{justifyContent:'center', alignItems:'center'}}>
+          <Text style={{marginVertical:12, textAlign:'center'}}>Delete this subject?</Text> 
+          <Text style={{marginBottom:24 ,textAlign:'center'}}>All notes will be lost and cannot be recovered</Text>
+
+          <Text style={{  marginBottom:4}}>Type DELETE to confirm</Text>  
+          <Input 
+          value={value}
+          onChangeText={nextValue => setValue(nextValue)}/>
+          <View style={{flexDirection:'row', marginTop:20,marginBottom:8}}>
+       
+          <Button status='danger' style={{marginRight:12}} onPress={()=>console.log('hello')}>
+          Delete
+          </Button>
+
+          <Button appearance='outline' onPress={()=>setVisible(false)}>
+          Close
+          </Button>
+          </View>
+        </View>
+        </Card>
+        </Modal>   
   
        </SafeAreaView>
       
@@ -108,10 +140,8 @@ const styles = StyleSheet.create({
   
   
 
-
-
-  container: {
-  
+  backdrop:{
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
   },
 
   contentContainer: {
