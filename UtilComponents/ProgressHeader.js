ae4bf599e859e33d5460eb694e88a3402d32d79f
Shing So <shingso@Shings-MacBook-Pro.local>
2020-12-03 19:50:13 -0800

    Added skeleton functions for Text and Picture return functions

--- UtilComponents/ProgressHeader.js
+++ UtilComponents/ProgressHeader.js
@@ -4,6 +4,44 @@ import { Button, Icon , TopNavigation, TopNavigationAction, Modal, Card, Text } 
 import { useNavigation, StackActions } from '@react-navigation/native';
 
 
+const getText = (number) => {
+
+
+    const messageDict = {
+
+        0: 'You have studied 2 times in the past week',
+        1: 'You have studied 2 times in the past week',
+        2: 'hello',
+        3: 'hello',
+        4: 'hello',
+        5: 'hello',
+        6: 'hello',
+        7: 'hello',
+
+    }
+
+    return messageDict[number]
+}
+
+const getPicture = (number) => {
+
+
+    const pictureDict = {
+
+        0: require('../assets/images/8600.5.png'),
+        1: require('../assets/images/8600.5.png'),
+        2: require('../assets/images/8600.5.png'),
+        3: require('../assets/images/8600.5.png'),
+        4: require('../assets/images/8600.5.png'),
+        5: require('../assets/images/8600.5.png'),
+        6: require('../assets/images/8600.5.png'),
+        7: require('../assets/images/8600.5.png'),
+
+    }
+
+    return pictureDict[number]
+}
+
 function ProgressHeader(props){
  
 
@@ -11,16 +49,16 @@ function ProgressHeader(props){
     // 1 times within the last two weeks - 
     // 4 times within the last two weeks - 
     // 7 times in the week? - Your killing it!  
-
     // depends on how many days have passed in the week 
-
     // need to set background pictures and text
+    // return how many times you studied in the past week vs how many days have passed in the week
 
     return (
        
       <Card>
-      <ImageBackground opacity={0.20} resizeMode='cover' source={require('../assets/images/8600.5.png')} style={styles.image}>
-      <Text>Hello</Text>
+      <ImageBackground opacity={0.20} resizeMode='cover' source={getPicture(props.messageNumber)} style={styles.image}>
+      <Text category='label'>{getText(props.messageNumber)}</Text>
+      <Text>You have been extremely consistent in studying. Keep going!</Text>
       </ImageBackground>
       </Card>
       
@@ -29,11 +67,11 @@ function ProgressHeader(props){
     }
 
 const styles = StyleSheet.create({
+  
   item: {
 
     marginVertical:8,
-  
-    
+   
   },
   //contanier that holds everything 
   
